ffdf76568c108e3196e29d2ecfa9971ec022ed8c
Abu Talha Ansari <101631188+GamerBhai02@users.noreply.github.com>
2025-03-06 06:53:53 +0530

    Update Set_update.py

--- plugins/Extra/Set_update.py
+++ plugins/Extra/Set_update.py
@@ -10,12 +10,12 @@ async def set_muc_id(client, message):
         if id and str(id).startswith('-100') and len(str(id)) == 14:
             is_suc = await db.movies_update_channel_id(id)
             if is_suc:
-                await message.reply("Successfully set movies update  channel id : " + id)
+                await message.reply("𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅𝗅𝗒 𝗌𝖾𝗍 𝗆𝗈𝗏𝗂𝖾𝗌 𝗎𝗉𝖽𝖺𝗍𝖾  𝖼𝗁𝖺𝗇𝗇𝖾𝗅 𝗂𝖽 : " + id)
             else:
-                await message.reply("Failed to set movies update channel id : " + id)
+                await message.reply("𝖥𝖺𝗂𝗅𝖾𝖽 𝗍𝗈 𝗌𝖾𝗍 𝗆𝗈𝗏𝗂𝖾𝗌 𝗎𝗉𝖽𝖺𝗍𝖾 𝖼𝗁𝖺𝗇𝗇𝖾𝗅 𝗂𝖽 : " + id)
         else:
-            await message.reply("Invalid channel id : " + id)
+            await message.reply("𝖨𝗇𝗏𝖺𝗅𝗂𝖽 𝖼𝗁𝖺𝗇𝗇𝖾𝗅 𝗂𝖽 : " + id)
     except Exception as e:
         print('Err in set_muc_id', e)
-        await message.reply("Failed to set movies channel id! Because : " + str(e))
+        await message.reply("𝖥𝖺𝗂𝗅𝖾𝖽 𝗍𝗈 𝗌𝖾𝗍 𝗆𝗈𝗏𝗂𝖾𝗌 𝖼𝗁𝖺𝗇𝗇𝖾𝗅 𝗂𝖽! 𝖡𝖾𝖼𝖺𝗎𝗌𝖾 : " + str(e))
 
